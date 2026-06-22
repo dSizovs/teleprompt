b96b2241f7e91e6564e56e06b1d9ddab9d1bfaad
dima <dmitrijs.sizovs@gmail.com>
2026-06-22 16:47:44 +0200

    Initial Commit

---
+++ teleprompt/telepromptApp.swift
@@ -0,0 +1,17 @@
+//
+//  telepromptApp.swift
+//  teleprompt
+//
+//  Created by Dmitrijs Sizovs on 22.06.26.
+//
+
+import SwiftUI
+
+@main
+struct telepromptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
